5e656bb60d9cc5d6544abbf77f0ce6a4e04ad1dd
Serenity <kim910320@naver.com>
2020-04-26 01:58:20 +0900

    [30. 스프링 웹 MVC 2부: HttpMessageConverter]
    - @ReuqestBody
    - @ResponseBody

--- 04부_스프링-부트-활용/29_스프링-웹-MVC/springbootgettingstarted/src/main/java/com/serenity/user/UserController.java
+++ 04부_스프링-부트-활용/29_스프링-웹-MVC/springbootgettingstarted/src/main/java/com/serenity/user/UserController.java
@@ -1,12 +1,16 @@
 package com.serenity.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) { // @ResponseBody 생략 가능
+        return user;
     }
 }
