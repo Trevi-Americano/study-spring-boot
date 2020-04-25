9fc77a94e01045ec55c7c2d28f7b58f959b82fab
Serenity <kim910320@naver.com>
2020-04-26 01:50:05 +0900

    [26. 테스트] @SpringBootTest
    - MOCK : 내장 Tomcat 사용 안 함

---
+++ 04부_스프링-부트-활용/26_테스트/springbootgettingstarted/src/main/java/com/serenity/sample/SampleService.java
@@ -0,0 +1,12 @@
+package com.serenity.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService
+{
+    public String getName()
+    {
+        return "serenity";
+    }
+}
