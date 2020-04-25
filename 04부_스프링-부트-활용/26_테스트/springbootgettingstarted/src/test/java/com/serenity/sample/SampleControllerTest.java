5c21614ca867c586ab94180d10b0fefacc35b344
Serenity <kim910320@naver.com>
2020-04-26 01:53:49 +0900

    [27. 테스트 유틸] OutputCapture
    - OutputCapture

--- 04부_스프링-부트-활용/26_테스트/springbootgettingstarted/src/test/java/com/serenity/sample/SampleControllerTest.java
+++ 04부_스프링-부트-활용/26_테스트/springbootgettingstarted/src/test/java/com/serenity/sample/SampleControllerTest.java
@@ -1,14 +1,17 @@
 package com.serenity.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,6 +20,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(SampleController.class)
 @AutoConfigureMockMvc
 public class SampleControllerTest {
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -29,5 +35,9 @@ public class SampleControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello serenity"));
+
+        assertThat(outputCapture.toString())
+                .contains("developer")
+                .contains("skip");
     }
 }
