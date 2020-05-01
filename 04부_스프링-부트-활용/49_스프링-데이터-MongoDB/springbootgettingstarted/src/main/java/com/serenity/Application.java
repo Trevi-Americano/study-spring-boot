2bde0b3851f4a643df311b50d20d56a7b69e70e1
Serenity <kim910320@naver.com>
2020-05-01 21:22:12 +0900

    [49. 스프링 데이터 10부: MongoDB]
    - MongoTemplate 사용

---
+++ 04부_스프링-부트-활용/49_스프링-데이터-MongoDB/springbootgettingstarted/src/main/java/com/serenity/Application.java
@@ -0,0 +1,32 @@
+package com.serenity;
+
+import com.serenity.account.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@SpringBootApplication
+public class Application {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            Account account = new Account();
+            account.setUsername("serenity");
+            account.setEmail("serenity@email.com");
+
+            mongoTemplate.insert(account);
+
+            System.out.println("finished");
+        };
+    }
+}
