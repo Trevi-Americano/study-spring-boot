08f6144a6b30e96b9e47cc15f01b20db8f296397
Serenity <kim910320@naver.com>
2020-05-05 01:58:16 +0900

    [52. 스프링 시큐리티 1부: Starter-Security]
    - Spring Security
    - Spring Security Test

---
+++ 04부_스프링-부트-활용/52_스프링-시큐리티/springbootgettingstarted/src/test/java/com/serenity/HomeControllerTest.java
@@ -0,0 +1,48 @@
+package com.serenity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello_without_user() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    @WithMockUser // 인증 정보
+    public void hel {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    @WithMockUser // 인증 정보
+    public void my() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+}
